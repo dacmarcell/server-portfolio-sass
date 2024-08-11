148a0365e3e7533e1676f020027782c151019a30
marcelldac <devmarcell@protonmail.com>
2024-08-10 21:24:25 -0300

    feat: create user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,21 @@
-export class User {}
+import { Column, PrimaryGeneratedColumn } from "typeorm"
+
+export class User {
+    @PrimaryGeneratedColumn('uuid')
+    user_id: string
+
+    @Column()
+    first_name: string
+
+    @Column()
+    last_name: string
+
+    @Column()
+    email: string
+
+    @Column()
+    password: string
+
+    @Column()
+    about: string
+}
