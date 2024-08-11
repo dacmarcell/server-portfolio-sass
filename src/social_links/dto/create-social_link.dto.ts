49723cb5ade2a7bdd98952e7e5c93aeaa0bf70a9
marcelldac <devmarcell@protonmail.com>
2024-08-10 21:46:34 -0300

    feat: create social link dto

--- src/social_links/dto/create-social_link.dto.ts
+++ src/social_links/dto/create-social_link.dto.ts
@@ -1,1 +1,18 @@
-export class CreateSocialLinkDto {}
+import { IsEnum, IsString, IsUrl } from "class-validator"
+
+enum SocialNetworks {
+    github = 'github',
+    linkedin = 'linkedin',
+    facebook = 'facebook',
+    twitter = 'twitter',
+}
+
+export class CreateSocialLinkDto {
+    @IsString()
+    @IsEnum(SocialNetworks)
+    name: string
+    
+    @IsString()
+    @IsUrl()
+    url: string
+}
