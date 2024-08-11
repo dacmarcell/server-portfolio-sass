3e384f899dfccf26b042dcee7804e85cbc5ff34a
marcelldac <devmarcell@protonmail.com>
2024-08-10 21:24:04 -0300

    feat: import type orm module globally

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { PostgresConfigService } from './config/postgres.config.service';
 
 @Module({
+  imports: [
+    UsersModule,
     ConfigModule.forRoot({
       isGlobal: true,
       cache: true
     }),
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresConfigService,
+      inject: [PostgresConfigService]
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
