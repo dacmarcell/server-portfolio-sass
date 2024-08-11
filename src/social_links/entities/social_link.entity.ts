ec7b41851984ad721db93dce0fa1bc662923df0d
marcelldac <devmarcell@protonmail.com>
2024-08-10 21:46:22 -0300

    feat: create social link entity

--- src/social_links/entities/social_link.entity.ts
+++ src/social_links/entities/social_link.entity.ts
@@ -1,1 +1,13 @@
-export class SocialLink {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({ name: "social_links" })
+export class SocialLink {
+    @PrimaryGeneratedColumn('increment')
+    social_link_id: string
+
+    @Column()
+    name: string
+    
+    @Column()
+    url: string
+}
