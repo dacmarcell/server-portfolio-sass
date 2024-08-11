deb90970dfb3296534d15226f995e7d48c94a8f7
marcelldac <devmarcell@protonmail.com>
2024-08-10 21:17:18 -0300

    feat: import config module globally

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule],
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true
+    }),
   controllers: [AppController],
   providers: [AppService],
 })
