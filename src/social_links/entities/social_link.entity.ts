6d6261e7a0f4bfc4938171da48c729e3f1c0a8c0
marcelldac <devmarcell@protonmail.com>
2024-08-10 21:51:58 -0300

    fix: change social link id entity type

--- src/social_links/entities/social_link.entity.ts
+++ src/social_links/entities/social_link.entity.ts
@@ -3,7 +3,7 @@ import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 @Entity({ name: "social_links" })
 export class SocialLink {
     @PrimaryGeneratedColumn('increment')
-    social_link_id: string
+    social_link_id: number
 
     @Column()
     name: string
