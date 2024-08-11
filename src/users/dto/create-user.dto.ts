f6a4d9c63dd817e43fe880365fb26ffc16081d32
marcelldac <devmarcell@protonmail.com>
2024-08-10 21:25:14 -0300

    feat: create user dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,24 @@
-export class CreateUserDto {}
+import { IsEmail, IsString, IsStrongPassword, Length } from "class-validator"
+
+export class CreateUserDto {
+    @Length(2, 30)
+    @IsString()
+    first_name: string
+
+    @Length(2, 30)
+    @IsString()
+    last_name: string
+
+    @Length(5, 50)
+    @IsEmail()
+    @IsString()
+    email: string
+
+    @IsStrongPassword()
+    @IsString()
+    password: string
+
+    @Length(200, 600)
+    @IsString()
+    about: string
+}
