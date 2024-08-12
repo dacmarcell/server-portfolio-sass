99df0c8916cf7c4251bc144d001622a5bd237c94
marcelldac <devmarcell@protonmail.com>
2024-08-11 22:24:04 -0300

    feat: add fields of expertise entity

--- src/fields_of_expertise/entities/fields_of_expertise.entity.ts
+++ src/fields_of_expertise/entities/fields_of_expertise.entity.ts
@@ -1,1 +1,32 @@
-export class FieldsOfExpertise {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+export enum FieldsOfExpertiseEnum {
+    BackEnd = 'Back-end',
+    FrontEnd = 'Front-end',
+    FullStack = 'Full-stack',
+    Mobile = 'Mobile',
+    DevOps = 'DevOps',
+    DataScience = 'Data Science',
+    UIUX = 'UI/UX',
+    Cybersecurity = 'Cybersecurity',
+    AIML = 'AI/ML',
+    Blockchain = 'Blockchain',
+    IoT = 'IoT',
+    CloudComputing = 'Cloud Computing',
+    ARVR = 'AR/VR',
+    GameDevelopment = 'Game Development',
+    QuantumComputing = 'Quantum Computing',
+    BigData = 'Big Data'
+}
+
+@Entity({name: 'fields_of_expertise' })
+export class FieldsOfExpertise {
+    @PrimaryGeneratedColumn('increment')
+    field_code: string
+
+    @Column({
+        type: 'enum',
+        enum: FieldsOfExpertiseEnum,
+    })
+    name: FieldsOfExpertiseEnum
+}
