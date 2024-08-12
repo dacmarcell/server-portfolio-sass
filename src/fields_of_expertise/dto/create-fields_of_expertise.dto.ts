34c6dd7a9ed69437fc5991dddd1501b8211c0666
marcelldac <devmarcell@protonmail.com>
2024-08-11 22:24:15 -0300

    feat: create fields of expertise create dto

--- src/fields_of_expertise/dto/create-fields_of_expertise.dto.ts
+++ src/fields_of_expertise/dto/create-fields_of_expertise.dto.ts
@@ -1,1 +1,7 @@
-export class CreateFieldsOfExpertiseDto {}
+import { IsEnum } from "class-validator";
+import { FieldsOfExpertiseEnum } from "../entities/fields_of_expertise.entity";
+
+export class CreateFieldsOfExpertiseDto {
+    @IsEnum(FieldsOfExpertiseEnum)
+    name: FieldsOfExpertiseEnum
+}
