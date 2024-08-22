93645c19f917e1238109525878342dc246781614
Developer Marcell <devmarcell@protonmail.com>
2024-08-22 12:14:53 -0300

    feat: import programming languages module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,9 +4,14 @@ import { UsersController } from './users.controller';
 import { FieldsOfExpertiseModule } from 'src/fields_of_expertise/fields_of_expertise.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { ProgrammingLanguagesModule } from 'src/programming_languages/programming_languages.module';
 
 @Module({
-  imports: [FieldsOfExpertiseModule, TypeOrmModule.forFeature([User])],
+  imports: [
+    FieldsOfExpertiseModule,
+    ProgrammingLanguagesModule,
+    TypeOrmModule.forFeature([User])
+  ],
   controllers: [UsersController],
   providers: [UsersService],
 })
