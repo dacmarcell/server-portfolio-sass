fb11551a799c3980be173ac4602a0aceed6310ce
Developer Marcell <devmarcell@protonmail.com>
2024-08-22 12:33:11 -0300

    refactor: change path of pg config service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-import { PostgresConfigService } from './config/postgres.config.service';
+import { PostgresConfigService } from './config/db/postgres.config.service';
 import { SocialLinksModule } from './social_links/social_links.module';
 import { ProgrammingLanguagesModule } from './programming_languages/programming_languages.module';
 import { ProjectsModule } from './projects/projects.module';
