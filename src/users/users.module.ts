cef3a6631d4af7688af48c13cc32bff25ba4f7b3
Developer Marcell <devmarcell@protonmail.com>
2024-08-22 12:23:14 -0300

    feat: import relationship modules

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,11 +5,15 @@ import { FieldsOfExpertiseModule } from 'src/fields_of_expertise/fields_of_exper
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { ProgrammingLanguagesModule } from 'src/programming_languages/programming_languages.module';
+import { ProjectsModule } from 'src/projects/projects.module';
+import { SocialLinksModule } from 'src/social_links/social_links.module';
 
 @Module({
   imports: [
     FieldsOfExpertiseModule,
     ProgrammingLanguagesModule,
+    ProjectsModule,
+    SocialLinksModule,
     TypeOrmModule.forFeature([User])
   ],
   controllers: [UsersController],
