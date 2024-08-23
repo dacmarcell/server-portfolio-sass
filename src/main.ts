2464afcdeddb9b1aea40314b911e96909571e1cc
Developer Marcell <devmarcell@protonmail.com>
2024-08-23 02:19:55 -0300

    feat: add console log to display app launch information

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 const port = process.env.PORT || 3000
+console.log(`Launching NestJS app on port ${port}, URL: http://0.0.0.0:${port}`)
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
